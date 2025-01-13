568a99a0a2da50e790d9e3deab7e0aa2c4f74cf2
PanchiAnubis <f.rivas06@ufromail.cl>
2025-01-13 14:16:49 -0300

    feat(ui): agregar componente primary button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   title = signal('e-commerce');
+  cart = signal("Cart");
+  showButtonClicked(){
+    console.log('Button clicked');
+  }
 }
